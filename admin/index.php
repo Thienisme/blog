8755d110694f3add5123f6852fee15b69c91bf6c
Thienisme <truongvanthien2k1@gmail.com>
2023-06-25 11:54:43 +0700

    prettier code

--- admin/index.php
+++ admin/index.php
@@ -9,112 +9,114 @@ $posts = mysqli_query($connection, $query);
 
 
 <section class="dashboard">
-<?php if (isset($_SESSION['add-post-success'])) : //hiển thị nếu thêm post thành công  ?>
-            <div class="alert__message success container">
-                <p>
-                <?= 
-                    $_SESSION['add-post-success'];
-                    unset($_SESSION['add-post-success']);
+    <?php if (isset($_SESSION['add-post-success'])) : //hiển thị nếu thêm post thành công  
+    ?>
+        <div class="alert__message success container">
+            <p>
+                <?=
+                $_SESSION['add-post-success'];
+                unset($_SESSION['add-post-success']);
                 ?>
-                </p>
-            </div>
-<?php elseif (isset($_SESSION['edit-post-success'])) : //hiển thị nếu sửa post thành công  ?>
-            <div class="alert__message success container">
-                <p>
-                <?= 
-                    $_SESSION['edit-post-success'];
-                    unset($_SESSION['edit-post-success']);
+            </p>
+        </div>
+    <?php elseif (isset($_SESSION['edit-post-success'])) : //hiển thị nếu sửa post thành công  
+    ?>
+        <div class="alert__message success container">
+            <p>
+                <?=
+                $_SESSION['edit-post-success'];
+                unset($_SESSION['edit-post-success']);
                 ?>
-                </p>
-            </div>
-<?php elseif (isset($_SESSION['edit-post'])) : //hiển thị nếu sửa post không thành công  ?>
-            <div class="alert__message error container">
-                <p>
-                <?= 
-                    $_SESSION['edit-post'];
-                    unset($_SESSION['edit-post']);
+            </p>
+        </div>
+    <?php elseif (isset($_SESSION['edit-post'])) : //hiển thị nếu sửa post không thành công  
+    ?>
+        <div class="alert__message error container">
+            <p>
+                <?=
+                $_SESSION['edit-post'];
+                unset($_SESSION['edit-post']);
                 ?>
-                </p>
-            </div>
-<?php elseif (isset($_SESSION['delete-post-success'])) : //hiển thị nếu xóa post thành công  ?>
-            <div class="alert__message success container">
-                <p>
-                <?= 
-                    $_SESSION['delete-post-success'];
-                    unset($_SESSION['delete-post-success']);
+            </p>
+        </div>
+    <?php elseif (isset($_SESSION['delete-post-success'])) : //hiển thị nếu xóa post thành công  
+    ?>
+        <div class="alert__message success container">
+            <p>
+                <?=
+                $_SESSION['delete-post-success'];
+                unset($_SESSION['delete-post-success']);
                 ?>
-                </p>
-            </div>
-<?php endif ?>
+            </p>
+        </div>
+    <?php endif ?>
     <div class="container dashboard__container">
         <aside>
             <ul>
                 <li><a href="add-post.php"><i class="uil uil-pen"></i>
-                    <h5>Add Post</h5>
-                </a>
+                        <h5>Add Post</h5>
+                    </a>
                 </li>
                 <li><a href="index.php" class="active"><i class="uil uil-postcard"></i>
-                <h5>Manage Posts</h5>
-                </a>
+                        <h5>Manage Posts</h5>
+                    </a>
                 </li>
-                <?php if(isset($_SESSION['user_is_admin'])): ?>
-                <li><a href="add-user.php"><i class="uil uil-user-plus"></i>
-                <h5>Add User</h5>
-                </a>
-                </li>
-                <li><a href="manage-users.php"><i class="uil uil-users-alt"></i>
-            <h5>Manage Users</h5>
-                </a>
-                </li>
-                <li><a href="add-category.php"><i class="uil uil-edit"></i>
-                    <h5>Add Category</h5>
+                <?php if (isset($_SESSION['user_is_admin'])) : ?>
+                    <li><a href="add-user.php"><i class="uil uil-user-plus"></i>
+                            <h5>Add User</h5>
                         </a>
-                </li>
-                <li><a href="manage-categories.php" ><i class="uil uil-list-ul"></i>
-                    <h5>Manage Categories</h5>
+                    </li>
+                    <li><a href="manage-users.php"><i class="uil uil-users-alt"></i>
+                            <h5>Manage Users</h5>
                         </a>
-                </li>
-                <?php endif?>
+                    </li>
+                    <li><a href="add-category.php"><i class="uil uil-edit"></i>
+                            <h5>Add Category</h5>
+                        </a>
+                    </li>
+                    <li><a href="manage-categories.php"><i class="uil uil-list-ul"></i>
+                            <h5>Manage Categories</h5>
+                        </a>
+                    </li>
+                <?php endif ?>
             </ul>
         </aside>
         <main>
             <h2>Manage Post</h2>
             <?php if (mysqli_num_rows($posts) > 0) : ?>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Category</th>
-                        <th>Status</th>
-                        <th>Created at</th>
-                        <th>Edit</th>
-                        <th>Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php while ($post = mysqli_fetch_assoc($posts)) : ?>
-                        <!-- get category title of each post from categories table -->
-                        <?php 
-                        $category_id = $post['category_id'];
-                        if (!is_null($category_id)) {
-                            $category_query = "SELECT title FROM categories WHERE id = $category_id";
-                            $category_result = mysqli_query($connection, $category_query);   
-                            $category = mysqli_fetch_assoc($category_result);
-                        }
-                        ?>
-                    <tr>
-                        <td><?= $post['title'] ?></td>
-                        <td><?= is_null($category_id) ? 'Uncategory' : $category['title'] ?></td>
-                        <td><?= $post['is_published'] == 1 ? 'Published' : 'Unpublished' ?></td>
-                        <td><?= date("M d, Y - H:i", strtotime($post['date_time'])) ?></td>
-                        <td><a href="<?= ROOT_URL ?>admin/edit-post.php?id=<?= $post['id'] ?>" 
-                        class="btn sm">Edit</a></td>
-                        <td><a href="<?= ROOT_URL ?>admin/delete-post.php?id=<?= $post['id'] ?>" 
-                        class="btn sm danger">Delete</a></td>                        
-                    </tr>
-                    <?php endwhile ?>
-                </tbody>
-            </table>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Category</th>
+                            <th>Status</th>
+                            <th>Created at</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php while ($post = mysqli_fetch_assoc($posts)) : ?>
+                            <!-- get category title of each post from categories table -->
+                            <?php
+                            $category_id = $post['category_id'];
+                            if (!is_null($category_id)) {
+                                $category_query = "SELECT title FROM categories WHERE id = $category_id";
+                                $category_result = mysqli_query($connection, $category_query);
+                                $category = mysqli_fetch_assoc($category_result);
+                            }
+                            ?>
+                            <tr>
+                                <td><?= $post['title'] ?></td>
+                                <td><?= is_null($category_id) ? 'Uncategory' : $category['title'] ?></td>
+                                <td><?= $post['is_published'] == 1 ? 'Published' : 'Unpublished' ?></td>
+                                <td><?= date("M d, Y - H:i", strtotime($post['date_time'])) ?></td>
+                                <td><a href="<?= ROOT_URL ?>admin/edit-post.php?id=<?= $post['id'] ?>" class="btn sm">Edit</a></td>
+                                <td><a href="<?= ROOT_URL ?>admin/delete-post.php?id=<?= $post['id'] ?>" class="btn sm danger">Delete</a></td>
+                            </tr>
+                        <?php endwhile ?>
+                    </tbody>
+                </table>
             <?php else : ?>
                 <div class="alert__messager error"><?= "No posts found" ?></div>
             <?php endif ?>
